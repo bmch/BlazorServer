17656966aa18dd0eb2138dfabf9f9531f69b22ec
Brendan McHugh <brendanmchugh@Brendans-Air.lan>
2021-09-27 14:46:33 +0100

    admin panels fines

--- Notes.Core/Note.cs
+++ Notes.Core/Note.cs
@@ -7,7 +7,16 @@ namespace Notes.Core
         [BsonId]
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string Id { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
+        public string Reference { get; set; }
+        public string PersonName { get; set; }
+        public string Status { get; set; }
+        public int Amount { get; set; }
+
+
+        public string Address1 { get; set; }  
+        public string Address2 { get; set; }  
+        public string City { get; set; }  
+        public string County { get; set; }  
+        public string Postcode { get; set; }
     }
 }
