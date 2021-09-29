43534ccc0b27cd126fe1db1b06228bf19891b38f
Brendan McHugh <brendanmchugh@Brendans-Air.lan>
2021-09-29 22:28:18 +0100

    update fines table and confirmation

--- Notes.Core/Note.cs
+++ Notes.Core/Note.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 
 namespace Notes.Core
 {
@@ -10,9 +11,14 @@ namespace Notes.Core
         public string Reference { get; set; }
         public string PersonName { get; set; }
         public string Status { get; set; }
-        public int Amount { get; set; }
-
-
+        public double Amount { get; set; }
+        public double AmountRepaid { get; set; }
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime FineIssuedDate { get; set; }
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime TransactionDate { get; set; }
+        public string TxRef { get; set; }  
+        public string Email { get; set; }  
         public string Address1 { get; set; }  
         public string Address2 { get; set; }  
         public string City { get; set; }  
